5272f9a48e4d86c51d40caa4eed2d2826f45bdc8
Dima <dimakaddah@gmail.com>
2020-03-21 03:29:46 +0100

    somechanges in ex1

--- Week2/homework/exercise1/app.js
+++ Week2/homework/exercise1/app.js
@@ -17,11 +17,11 @@ const executeQuery = util.promisify(connection.query.bind(connection));
 
 async function seedDatabase() {
   const create_authors_table = `CREATE TABLE IF NOT EXISTS Authors (
-    author_no int NOT NULL AUTO_INCREMENT,
+    author_no int (4) ZEROFILL NOT NULL AUTO_INCREMENT,
     author_name varchar(50),
-    university varchar(50),
+    country varchar(50),
     date_of_birth DATE,
-    h_index int,
+    h_index int (8) ZEROFILL,
     gender enum('f','m'),
     CONSTRAINT PK_Authors PRIMARY KEY (author_no));`;
   const add_friend_column = `ALTER TABLE Authors ADD COLUMN Friend int;`;
